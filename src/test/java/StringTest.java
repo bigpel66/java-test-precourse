5dcbf4f91972cb1b405ff9857184e6e3389d1742
bigpel66 <bigpel66@icloud.com>
2024-03-16 01:01:03 +0900

    test(StringTest): 범위를 벗어난 인덱스를 사용 시 예외 발생 확인 - charAt 메서드 사용하여 범위를 벗어난 인덱스 사용 시 IndexOutOfBoundsException 예외 발생 - assertThatThrownBy와 isInstanceOf를 사용하여 검증

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("학습 테스트 - 문자열")
 public class StringTest {
@@ -35,6 +36,13 @@ public class StringTest {
 		for (int i = 0; i < given.length(); i++) {
 			assertThat(given.charAt(i)).isEqualTo(expected[i]);
 		}
+	}
+
+	@Test
+	@DisplayName("문자열 범위 밖의 특정 문자 확인 시 예외 발생을 확인")
+	void checkCharAtException() {
+		String given = "abc";
+		assertThatThrownBy(() -> given.charAt(given.length() + 1)).isInstanceOf(IndexOutOfBoundsException.class);
 	}
 
 }
