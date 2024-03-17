f7053e513283f1a108937925b9f588f761c53a7e
bigpel66 <bigpel66@icloud.com>
2024-03-17 18:00:48 +0900

    test(SetTest): 집합 요소 보유 여부 확인 - ParameterizedTest와 CsvSource를 이용 - 1, 2, 3 보유를 확인 - 4, 5 존재하지 않음을 확인 - 기타 정렬되지 않은 소스 코드 수정

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,14 +1,14 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("학습 테스트 - 집합")
 public class SetTest {
@@ -31,10 +31,17 @@ public class SetTest {
 	}
 
 	@ParameterizedTest
-	@DisplayName("집합 요소 존재 여부 확인")
+	@DisplayName("집합 요소 존재 여부 확인 - True 케이스 (ValueSource)")
 	@ValueSource(ints = {1, 2, 3})
 	void hasElementOnSet(int expectedElement) {
 		assertThat(numbers.contains(expectedElement)).isTrue();
+	}
+
+	@ParameterizedTest
+	@DisplayName("집합 요소 존재 여부 확인 - True & False 케이스 (CsvSource)")
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void checkElementOnSet(int expectedElement, boolean isExist) {
+		assertThat(numbers.contains(expectedElement)).isEqualTo(isExist);
 	}
 
 }
