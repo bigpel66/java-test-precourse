cfd03167eb35111bd505ff9ae7064658354aff57
bigpel66 <bigpel66@icloud.com>
2024-03-17 17:54:15 +0900

    test(SetTest): 집합 요소 보유 여부 확인 - ParameterizedTest와 ValueSource를 이용 - 1, 2, 3 보유를 확인

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -2,6 +2,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -26,6 +28,13 @@ public class SetTest {
 	@DisplayName("집합의 사이즈를 확인")
 	void checkSetSize() {
 		assertThat(numbers.size()).isEqualTo(3);
+	}
+
+	@ParameterizedTest
+	@DisplayName("집합 요소 존재 여부 확인")
+	@ValueSource(ints = {1, 2, 3})
+	void hasElementOnSet(int expectedElement) {
+		assertThat(numbers.contains(expectedElement)).isTrue();
 	}
 
 }
