61bfb355df4e048a1147eb5b70955c2589a2078a
bigpel66 <bigpel66@icloud.com>
2024-03-17 17:49:55 +0900

    test(SetTest): 집합 크기 확인 테스트 - Collection Set::size를 이용하여 크기 확인 작성

--- src/test/java/SetTest.java
+++ src/test/java/SetTest.java
@@ -1,5 +1,31 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("학습 테스트 - 집합")
 public class SetTest {
+
+	private Set<Integer> numbers;
+
+	@BeforeEach
+	void setUp() {
+		numbers = new HashSet<>();
+		numbers.add(1);
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+	}
+
+	@Test
+	@DisplayName("집합의 사이즈를 확인")
+	void checkSetSize() {
+		assertThat(numbers.size()).isEqualTo(3);
+	}
+
 }
